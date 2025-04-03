2707892bfc2ac1105ffb82101e4ac7867ee8da00
GabrielPresense <gabrielpresense@gmail.com>
2025-04-03 08:50:46 -0300

    Console.log

---
+++ JavaScript/7. Console.log/index.js
@@ -0,0 +1,9 @@
+console.log(); // Exibir informações na tela com
+console.log('Gabriel Presense'); // Aspas simples
+console.log("Gabriel Presense"); // Aspas duplas
+console.log(`Gabriel Presense`); // e Crases
+console.log('Gabriel "Presense"'); 
+console.log("Gabriel 'Presense'");   //Texto = String
+console.log(`'Gabriel' "Presense"`); //Número = Number
+
+console.log(35, 15.85, 'Gabriel Presense De Novo');
