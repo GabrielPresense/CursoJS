f57b27a127974215d04d55175dd65d7726a2a42d
GabrielPresense <gabrielpresense@gmail.com>
2025-04-03 16:30:48 -0300

    Comentários de código

---
+++ JavaScript/11. Comentários de código/index.js
@@ -0,0 +1,21 @@
+// Comentario de linha // - Comentário de bloco /* Comente o que for preciso */
+// E escrever um comentário longo
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1'); 
+// Comentário
+
+/*
+Um bloco de código
+Linha de comentário
+*/
+
+/**
+ * Um título
+ * 
+ * Lorem ipsum
+ */
+
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
