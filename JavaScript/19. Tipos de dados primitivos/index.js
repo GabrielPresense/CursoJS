ebe6aef283e35158fdc5f5bbbe29fab18594c096
GabrielPresense <gabrielpresense@gmail.com>
2025-04-04 08:57:59 -0300

    Tipos de dados primitivos

---
+++ JavaScript/19. Tipos de dados primitivos/index.js
@@ -0,0 +1,19 @@
+// Tipos de dados primitivos - String, number, underfined, object/null, boolean
+const nome = 'Gabriel'; // String
+const nome1 = "Gabriel"; // String
+const nome2 = `Gabriel`; // String
+
+const num1 = 10; // Number
+const num2 = 10.52; // Number
+
+let nomeAluno; // Undefined -> Não coloca nenhum valor na variável
+const sobrenomeAluno = null; // Nulo, deixo explicito que nao aponta para local nenhum na memória
+
+const aprovado = false // Boolean = true, false (lógico)
+
+console.log(typeof nome, nome);
+console.log(typeof num1, num1);
+console.log(typeof num2, num2);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
