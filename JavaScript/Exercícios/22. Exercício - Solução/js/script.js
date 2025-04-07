290c882c1ca02413828c5b5f162ada38bc509868
GabrielPresense <gabrielpresense@gmail.com>
2025-04-07 07:30:03 -0300

    Exercício - Solução

---
+++ JavaScript/Exercícios/22. Exercício - Solução/js/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Digite um némuero');
+let num2 = prompt('Digite ou segundo número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi: ${ num1 + num2}`);
+ 
