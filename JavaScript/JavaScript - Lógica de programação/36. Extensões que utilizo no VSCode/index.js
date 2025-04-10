5bde4d4be5c64fc8fa8513216b8b7214d0daa3a2
GabrielPresense <gabrielpresense@gmail.com>
2025-04-10 08:55:35 -0300

    Extensões que utilizo no VSCode

---
+++ JavaScript/JavaScript - Lógica de programação/36. Extensões que utilizo no VSCode/index.js
@@ -0,0 +1,8 @@
+/**Extenções Recomendadas
+ * Code Runner - Executa os Script 
+ * Code Time - Métrica de tempo
+ * EJS language support - Renderizar HTML pelo Express
+ * Dracula Official - Tema VsCode
+ * Material Icon Theme
+ * Live Server - Atualiza a página HTML automaticamente após salvar o arquivo
+ */
