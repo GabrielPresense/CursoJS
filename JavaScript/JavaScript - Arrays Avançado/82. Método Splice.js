ced889677558e797789c13867e07146460325752
Gabriel Presense <gabrielpresense@gmail.com>
2025-07-19 02:41:19 -0300

    Método Splice

---
+++ JavaScript/JavaScript - Arrays Avançado/82. Método Splice.js
@@ -0,0 +1,15 @@
+//                -5        -4       -3        -2         -1
+//                 0         1        2         3          4
+const nomes = ['Gabriel', 'Luana', 'Julia', 'Wallace', 'Rosana'];
+
+// Unshift
+nomes.splice(0, 0, 'Luiz', 'Otávio');
+console.log(nomes);
+
+//nome.splice(índice atual, delete, elem1, elem2, elem3);
+// pop
+//const removidos = nomes.splice(-1, 1);
+
+// shift
+//const removidos = nomes.splice(0, 1);
+
