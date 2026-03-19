883e139c75daba33523d475713c61e49fcad5beb
Gabriel Presense <gabrielpresense@gmail.com>
2026-03-19 17:22:05 -0300

    10. Comentários de código

---
+++ JavaScript/JavaScript Básico - Para iniciantes/10. Comentários de código.js
@@ -0,0 +1,24 @@
+// Comentário de linha
+// E escrever um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário
+
+/* Comentário de bloco
+Um bloco de código
+Linha de comentário
+*/
+
+/** Comentário de bloco estilizado
+ * Um título
+ * 
+ * Lorem ipsum 
+ */
+
+// console.log('Linha 2');
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+// Vai ser ignorado
+
