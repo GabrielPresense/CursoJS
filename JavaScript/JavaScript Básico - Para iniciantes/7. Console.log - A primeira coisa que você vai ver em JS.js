9eef2a8255ee9ef06b1afc905c5606260aed0f7e
Gabriel Presense <gabrielpresense@gmail.com>
2026-03-19 14:02:17 -0300

    7. Console.log - A primeira coisa que você vai ver em JS

---
+++ JavaScript/JavaScript Básico - Para iniciantes/7. Console.log - A primeira coisa que você vai ver em JS.js
@@ -0,0 +1,5 @@
+console.log("Gabriel 'Presense'"); // '', "", `` -> String = Texto
+console.log('Gabriel "Presense"');
+console.log(`'Gabriel' "Presense"`);
+
+console.log(35, 15.85, 'Gabriel Presense');
