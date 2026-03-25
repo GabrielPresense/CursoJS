6ec850f0417388905ddcb0ac428f1910678c716d
Gabriel Presense <gabrielpresense@gmail.com>
2026-03-25 14:09:16 -0300

    27. Objeto Math

---
+++ JavaScript/JavaScript Básico - Para iniciantes/27. Objeto Math/js/script.js
@@ -0,0 +1,11 @@
+let num1 = 25;
+console.log(num1 ** 0.5);
+
+// let num2 = Math.floor(num1); // Arredonda para baixo
+// let num2 = Math.ceil(num1); // Arredonda para cima
+// let num2 = Math.round(num1); // Arredonda para o mais proximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Pega o maior numero de uma lista
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Pega o menor numero de uma lista
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Gere numeros aleatórios
+// console.log(100 / 0); // Infinity
+// console.log(!!(100 / 0));
