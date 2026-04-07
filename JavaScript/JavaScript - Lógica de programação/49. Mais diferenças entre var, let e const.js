d118b5c5312d6890c1ad57dde516acbdc6ea4259
GabrielPresense <134865625+GabrielPresense@users.noreply.github.com>
2026-04-07 16:48:31 -0300

    49. Mais diferenças entre var, let e const

---
+++ JavaScript/JavaScript - Lógica de programação/49. Mais diferenças entre var, let e const.js
@@ -0,0 +1,40 @@
+const verdadeira = true;
+
+// Let ten escopo de bloco { ... bloco }
+// Var só tem escopo de função
+
+// let nome = 'Gabriel'; // criando
+// var nome2 = 'Gabriel'; // criando
+
+// if (verdadeira) {
+//     let nome = 'Presense'; // criando
+//     var nome2 = 'Pedro'; // Redeclarando
+
+//     if (verdadeira) {
+//         var nome2 = 'João' // Redeclarando
+//         let nome = 'Outra coisa';
+//     }
+// }
+
+// console.log(nome, nome2);
+
+// ---------------------------------------
+
+// function falaOi () {
+    
+//     if (verdadeira) {
+//         let nome = 'Gabriel'
+//         var sobrenome = 'Presense';
+//     }
+
+//     console.log(nome)
+
+// }
+
+// falaOi()
+
+// ---------------------------------------
+
+console.log(sobrenome);
+
+let sobrenome = 'Presense';
