b3f7760828241978a248cc3bb66081f6bb3975cb
GabrielPresense <134865625+GabrielPresense@users.noreply.github.com>
2026-04-08 11:48:22 -0300

    52. For - Clássico - Estrutura de repetição

---
+++ JavaScript/JavaScript - Lógica de programação/52. For - Clássico - Estrutura de repetição.js
@@ -0,0 +1,35 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// De 1 em 1
+// for (let i = 0; i <= 500; i++) {
+//     console.log(`Linha ${i}`);
+// }
+
+// De 10 em 10
+// for (let i = 0; i <= 500; i += 10) {
+//     console.log(`Linha ${i}`);
+// }
+
+// Regredindo de 10 em 10
+// for (let i = 500; i >= 400; i -= 10) {
+//     console.log(`Linha ${i}`);
+// }
+
+// Par ou impar 
+// for (let i = 0; i <= 10; i ++) {
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i, par);
+// }
+
+
+//                 0       1      2 ...
+const frutas = ['Maça', 'Pêra', 'Uva', 'Amora', 'Brocolis', 'Caju', 'Goiaba'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]); 
+}
