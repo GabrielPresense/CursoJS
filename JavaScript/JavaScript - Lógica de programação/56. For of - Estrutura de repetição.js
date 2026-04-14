d42252aa58a194d0d6ba9ed7b8c43ca5fe5cbf2d
GabrielPresense <134865625+GabrielPresense@users.noreply.github.com>
2026-04-14 17:23:20 -0300

    56. For of - Estrutura de repetição

---
+++ JavaScript/JavaScript - Lógica de programação/56. For of - Estrutura de repetição.js
@@ -0,0 +1,30 @@
+const pessoa = {
+    nome: 'Gabriel',
+    sobrenome: 'Otávio'
+};
+
+// For clássico - geralmente com iteráveis (array ou string)
+// For in - Retorna índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
+
+// for (let i = 0; i < nomes.length; i++) {
+//     console.log(nomes[i]);
+// }
+
+// const nomes = ['Gabriel', 'Luana', 'Julia'];
+
+// for (let valor of nomes) {
+//     console.log(valor);
+// }
+
+// console.log('####');
+
+// for (let i in nomes) {
+//     console.log(nomes[i]);
+// }
+
+// console.log('####');
+
+// nomes.forEach(function(valor, indice, array) {
+//     console.log(valor, indice, array);
+// });
