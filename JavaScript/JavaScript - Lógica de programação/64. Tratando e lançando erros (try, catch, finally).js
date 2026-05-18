7acdf6024c979b53d32bc5faba189c719d2fbdc1
GabrielPresense <134865625+GabrielPresense@users.noreply.github.com>
2026-05-18 16:37:30 -0300

    64. Tratando e lançando erros (try, catch, finally)

---
+++ JavaScript/JavaScript - Lógica de programação/64. Tratando e lançando erros (try, catch, finally).js
@@ -0,0 +1,58 @@
+/*
+try {
+    // É executada quando não há erros
+} catch (e) {
+    // É executada quando há erros
+} finally {
+    // Sempre
+}
+*/
+
+/*
+try {
+    console.log(a);
+    console.log('Abrir um arquivo');
+    console.log('Manipulei o arquivo e gerou o erro');
+    console.log('Fechei o arquivo');
+
+    try {
+        console.log(b);
+    } catch(e) {
+        console.log('Deu erro');
+    } finally {
+        console.log('Também sou finally');
+    }
+
+} catch (e) {
+    console.log('Tratando o erro');
+} finally {
+    console.log('FINALLY: Eu sempre sou executado');
+}
+*/
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date.');
+    }
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+try {
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora(11);
+    console.log(hora);
+} catch(e) {
+    // Tratar erro
+    // coneole.log(e)
+} finally {
+    console.log('Tenha um bom dia.')
+}
