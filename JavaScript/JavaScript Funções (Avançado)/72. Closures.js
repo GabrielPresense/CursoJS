af66ffa4e1a6d314b8f9eeae04aaa9a3f79b7464
GabrielPresense <134865625+GabrielPresense@users.noreply.github.com>
2026-05-26 13:46:05 -0300

    72. Closures

---
+++ JavaScript/JavaScript Funções (Avançado)/72. Closures.js
@@ -0,0 +1,13 @@
+// Global
+function retornaFuncao(nome) {
+    return function () {
+        return nome;
+    };
+}
+
+const funcao = retornaFuncao('Gabriel');
+const funcao2 = retornaFuncao('João');
+console.dir(funcao);
+console.dir(funcao2);
+
+console.log(funcao(), funcao2());
