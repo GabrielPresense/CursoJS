b2d0cb3afe830983fb0c468786e796d5e3c6eada
GabrielPresense <134865625+GabrielPresense@users.noreply.github.com>
2026-07-10 16:31:06 -0300

    81. Revisão básico em Arrays

---
+++ JavaScript/JavaScript Funções (Avançado)/81. Revisão básico em Arrays.js
@@ -0,0 +1,11 @@
+// Valor por referência
+//                  0        1        2         3          4 
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+// const nome = 'Gabriel Presense';
+// const nomes = nome.split(' ');
+const nomes = [ 'Gabriel', 'Presense' ];
+const nome = nomes.join(' ');
+console.log(nome);
